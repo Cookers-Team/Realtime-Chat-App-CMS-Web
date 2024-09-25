4f6bd9a97db8f30a0ccfd65ccdd101593fb0b156
trong.kd <kienductrong@gmail.com>
2024-09-25 09:00:05 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./views/Login";
+import Loading from "./views/Loading";
+import User from "./views/User";
+import useFetch from "./hooks/useFetch";
+import NotFound from "./views/NotFound";
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const { post, loading } = useFetch();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await localStorage.getItem("accessToken");
+      const res = await post("/v1/user/verify-token", { accessToken: token });
+      if (res.result) {
+        setIsAuthenticated(true);
+      } else {
+        await localStorage.removeItem("accessToken");
+        setIsAuthenticated(false);
+      }
+    };
+    checkToken();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={isAuthenticated ? <User /> : <Login />}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </>
+      )}
+    </>
+  );
+};
+
+export default App;
