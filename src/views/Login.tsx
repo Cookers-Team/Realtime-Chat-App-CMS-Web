8374b6625c5f9ef4379a3370798349f488a1865e
trong.kd <kienductrong@gmail.com>
2024-11-17 23:22:36 +0700

    Update Login.tsx

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,4 +1,4 @@
-import { LockIcon, MailIcon, UserCircleIcon } from "lucide-react";
+import { LockIcon, UserCircleIcon } from "lucide-react";
 import InputField from "../components/InputField";
 import { useState } from "react";
 import useForm from "../hooks/useForm";
@@ -45,35 +45,28 @@ const Login = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full">
         <h2 className="text-2xl font-bold text-center mb-6">Đăng nhập</h2>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            handleSubmit();
-          }}
-        >
-          <InputField
-            title="Tài khoản đăng nhập"
-            isRequire={true}
-            placeholder="Nhập email, SĐT hoặc MSSV"
-            onChangeText={(value: any) => handleChange("username", value)}
-            value={form.username}
-            icon={UserCircleIcon}
-            error={errors.username}
-          />
-          <InputField
-            title="Mật khẩu"
-            isRequire={true}
-            placeholder="Nhập mật khẩu"
-            onChangeText={(value: any) => handleChange("password", value)}
-            value={form.password}
-            icon={LockIcon}
-            secureTextEntry={!showPassword}
-            togglePassword={() => setShowPassword(!showPassword)}
-            showPassword={showPassword}
-            error={errors.password}
-          />
-          <Button title="ĐĂNG NHẬP" color="royalblue" onPress={handleSubmit} />
-        </form>
+        <InputField
+          title="Tài khoản đăng nhập"
+          isRequire={true}
+          placeholder="Nhập email, SĐT hoặc MSSV"
+          onChangeText={(value: any) => handleChange("username", value)}
+          value={form.username}
+          icon={UserCircleIcon}
+          error={errors.username}
+        />
+        <InputField
+          title="Mật khẩu"
+          isRequire={true}
+          placeholder="Nhập mật khẩu"
+          onChangeText={(value: any) => handleChange("password", value)}
+          value={form.password}
+          icon={LockIcon}
+          secureTextEntry={!showPassword}
+          togglePassword={() => setShowPassword(!showPassword)}
+          showPassword={showPassword}
+          error={errors.password}
+        />
+        <Button title="ĐĂNG NHẬP" color="royalblue" onPress={handleSubmit} />
       </div>
       <LoadingDialog isVisible={loading} />
       <ToastContainer />
